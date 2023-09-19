9d4f57398981f7e74b3904e94d891501047a140c
sonenagisa <csna20061@g.nihon-u.ac.jp>
2023-09-19 00:04:40 +0000

    1st

--- main.cpp
+++ main.cpp
@@ -7,12 +7,20 @@ Serial pc(USBTX, USBRX);
 
 int main() {
     while(1) {
+        wait(0.5);
         bmx.getAcc();
-        pc.printf("%2.4lf, %2.4lf, %2.4lf, ",(bmx.accel[0]/512)*9.8,(bmx.accel[1]/512)*9.8,(bmx.accel[2]/512)*9.8);
+        wait(0.5);
+        pc.printf("%2.4lf, %2.4lf, %2.4lf\r\n",(bmx.accel[0]/512)*9.8,(bmx.accel[1]/512)*9.8,(bmx.accel[2]/512)*9.8);
+        /*
+        wait(0.5);
         bmx.getGyro();
-        pc.printf("%2.4lf, %2.4lf, %2.4lf, ",(bmx.gyroscope[0]*125)/2048,(bmx.gyroscope[1]*125)/2048,(bmx.gyroscope[2]*125)/2048);       
+        wait(0.5);
+        pc.printf("%2.4lf, %2.4lf, %2.4lf\r\n",(bmx.gyroscope[0]*125)/2048,(bmx.gyroscope[1]*125)/2048,(bmx.gyroscope[2]*125)/2048);       
+        wait(0.5);
         bmx.getMag();
+        wait(0.5);
         pc.printf("%2.4lf, %2.4lf, %2.4lf\r\n",bmx.magnet[0],bmx.magnet[1],bmx.magnet[2]);
-        wait(0);
+        wait(0.5);
+        */
     }
 }
